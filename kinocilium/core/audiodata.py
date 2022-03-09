d96f9554c7cd78af2317bc9602f00e20fb18d317
anderzzz <ohrn.anders@gmail.com>
2022-03-09 13:32:24 +0100

    Provide file selection filter as glob pattern directly from api

--- kinocilium/core/audiodata.py
+++ kinocilium/core/audiodata.py
@@ -113,22 +113,18 @@ class AudioPlainWAVData(_AudioUnlabelledDataset):
     '''Dataset for unlabelled audio data
 
     Args:
-        path_to_folder (str):
-        filter_on_suffix (str): NEIN NEIN NEIN
+        path_to_folder (str): path to folder with audio files
+        file_pattern (str): Unix style file selection for which files in folder to include
         read_metadata (bool): if metadata for the files should be read and returned; if not `None` returned
 
     '''
-    def __init__(self, path_to_folder, filter_on_suffix,
+    def __init__(self, path_to_folder, file_pattern='*.wav',
                  read_metadata=True):
         p = Path(path_to_folder)
         if not p.is_dir():
             raise ValueError('File folder {} not found'.format(path_to_folder))
 
-        if filter_on_suffix is None:
-            filter_condition = '*'
-        else:
-            filter_condition = '*.{}'.format(filter_on_suffix)
-        file_paths = list(p.glob(filter_condition))
+        file_paths = list(p.glob(file_pattern))
         n_file_paths = len(file_paths)
 
         super(AudioPlainWAVData, self).__init__(file_path_getter=lambda idx: file_paths[idx],
@@ -238,9 +234,9 @@ class AudioPlainWAVDataBuilder(object):
     def __init__(self):
         self._instance = None
 
-    def __call__(self, path_to_folder, filter_on_suffix='wav', read_metadata=True):
+    def __call__(self, path_to_folder, file_pattern='*.wav', read_metadata=True):
         self._instance = AudioPlainWAVData(path_to_folder=path_to_folder,
-                                           filter_on_suffix=filter_on_suffix,
+                                           file_pattern=file_pattern,
                                            read_metadata=read_metadata)
 
         return self._instance
