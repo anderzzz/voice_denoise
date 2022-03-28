fa9f8aeaffd55342442ed95da421a0fd91bf12ab
anderzzz <ohrn.anders@gmail.com>
2022-03-28 11:19:08 +0200

    Beginnings of custom criterion implementation

---
+++ kinocilium/core/loss_criteria.py
@@ -0,0 +1,24 @@
+'''Custom loss criteria
+
+'''
+from torch import nn
+
+class Source2DistortionRatio(nn.Module):
+    '''Bla bla
+
+    '''
+    def __init__(self):
+        super(Source2DistortionRatio, self).__init__()
+
+    def forward(self, x):
+        return x
+
+class ScaleInvariantSource2NoiseRatio(nn.Module):
+    '''Bla bla
+
+    '''
+    def __init__(self):
+        super(ScaleInvariantSource2NoiseRatio, self).__init__()
+
+    def forward(self, x):
+        return None
